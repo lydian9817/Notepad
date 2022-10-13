eec4e1fd65f8ed42fcf9bd4fc6bf6dec82ef5a13
lydian9817 <mauricio.caricari@gmail.com>
2022-10-13 18:21:27 -0300

    added nonnull annotation

--- app/src/main/java/com/example/notepad/data/database/Note.kt
+++ app/src/main/java/com/example/notepad/data/database/Note.kt
@@ -3,14 +3,11 @@ package com.example.notepad.data.database
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import javax.annotation.Nonnull
 
 @Entity(tableName = "note_table")
 data class Note(
-    @PrimaryKey(autoGenerate = true)
-    val id: Int = 0,
-    @ColumnInfo(name = "note_title")
-    val noteTitle: String,
-    @ColumnInfo(name = "note_content")
-    val noteContent: String,
-
+    @PrimaryKey(autoGenerate = true) val id: Int = 0,
+    @Nonnull @ColumnInfo(name = "note_title") val noteTitle: String,
+    @Nonnull @ColumnInfo(name = "note_content") val noteContent: String,
     )
