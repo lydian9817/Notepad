1be4a5d787ab3ccf33d02127743746a950dc3e98
lydian9817 <mauricio.caricari@gmail.com>
2022-10-13 18:02:14 -0300

    created RoomModule.kt

--- app/src/main/java/com/example/notepad/di/RoomModule.kt
+++ app/src/main/java/com/example/notepad/di/RoomModule.kt
@@ -1,4 +1,27 @@
 package com.example.notepad.di
 
-class RoomModule {
+import android.content.Context
+import androidx.room.Room
+import com.example.notepad.data.database.NoteDatabase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object RoomModule {
+    //module set up
+    private const val NOTE_DATABASE_NAME = "note_database"
+
+    @Singleton
+    @Provides
+    fun provideRoom(@ApplicationContext context: Context) =
+        Room.databaseBuilder(context, NoteDatabase::class.java, NOTE_DATABASE_NAME).build()
+
+    @Singleton
+    @Provides
+    fun provideNoteDao(db: NoteDatabase) = db.getNoteDao()
 }
