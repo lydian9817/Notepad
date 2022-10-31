0947bf6a95e99b80fd91cd36abce8c1a8f127986
mauri <mauricio.caricari@gmail.com>
2022-10-31 15:03:56 -0300

    Added IconRow composable

--- app/src/main/java/com/example/notepad/ui/edit/EditScreen.kt
+++ app/src/main/java/com/example/notepad/ui/edit/EditScreen.kt
@@ -3,14 +3,19 @@ package com.example.notepad.ui.edit
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
-import androidx.compose.material.Button
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Done
+import androidx.compose.material.icons.rounded.Menu
 import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.notepad.ui.NotepadViewModel
+import com.example.notepad.ui.home.AppBar
 import com.example.notepad.ui.notes.TextNoteEdit
 import com.example.notepad.ui.theme.NotepadTheme
 
@@ -20,7 +25,9 @@ fun EditScreen(onClickSaveNote: () -> Unit = {}, notepadViewModel: NotepadViewMo
     var noteContent by rememberSaveable { mutableStateOf("") }
     val isNoteValid = notepadViewModel.isNoteValid(noteTitle, noteContent)
 
+
     Column {
+        IconRow()
         TextNoteEdit(
             title = noteTitle,
             onTitleChange = { noteTitle = it },
@@ -29,6 +36,24 @@ fun EditScreen(onClickSaveNote: () -> Unit = {}, notepadViewModel: NotepadViewMo
         Spacer(modifier = Modifier.height(30.dp))
         SaveButton(onClickSave = onClickSaveNote, isEnabled = isNoteValid)
     }
+}
+
+@Composable
+fun IconRow() {
+    TopAppBar(
+        navigationIcon = {
+            IconButton(onClick = { /*TODO*/ }) {
+                Icon(
+                    imageVector = Icons.Rounded.Done,
+                    contentDescription = "Save button",
+                    modifier = Modifier.padding(horizontal = 12.dp)
+                )
+            }
+        },
+        title = {
+            Text("Edit Screen") //TODO poner la pantalla actual como string
+        }
+    )
 }
 
 @Composable
