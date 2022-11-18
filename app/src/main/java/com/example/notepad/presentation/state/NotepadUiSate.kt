9f56cc9d423574e00ac19df9ccce88cf281e85c2
mauri <mauricio.caricari@gmail.com>
2022-11-18 14:49:18 -0300

    created NotepadUiSate.kt class

--- app/src/main/java/com/example/notepad/presentation/state/NotepadUiSate.kt
+++ app/src/main/java/com/example/notepad/presentation/state/NotepadUiSate.kt
@@ -1,6 +1,11 @@
 package com.example.notepad.presentation.state
 
+import com.example.notepad.domain.model.Note
+import com.example.notepad.domain.util.NoteOrder
+import com.example.notepad.domain.util.OrderType
+
 data class NotepadUiSate(
-    val noteTitle: String = "",
-    val noteContent: String= ""
+    val notes: List<Note> = emptyList(),
+    val noteOrder: NoteOrder = NoteOrder.Date(OrderType.Descending), //default order
+    val isOrderSectionVisible: Boolean = false
 )
