28d9ae55e2bfd40888ae1e73aa085fd7e8b4218e
mauri <mauricio.caricari@gmail.com>
2022-11-22 10:10:22 -0300

    Added a dropdown menu

--- app/src/main/java/com/example/notepad/ui/home/HomeScreen.kt
+++ app/src/main/java/com/example/notepad/ui/home/HomeScreen.kt
@@ -1,22 +1,19 @@
 package com.example.notepad.ui.home
 
 import android.annotation.SuppressLint
+import android.widget.Toast
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.rounded.Add
-import androidx.compose.material.icons.rounded.Menu
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.State
+import androidx.compose.material.icons.rounded.*
+import androidx.compose.runtime.*
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import com.example.notepad.data.database.Note
 import com.example.notepad.ui.NotepadViewModel
@@ -28,11 +25,22 @@ fun HomeScreen(
     onNoteClick: (String) -> Unit = {},
     notepadViewModel: NotepadViewModel
 ) {
+    var isMenuOpen by remember { mutableStateOf(false) }
     val notes = notepadViewModel.allNotes.observeAsState(listOf())
     val isListEmpty = notepadViewModel.isListEmpty()
+
     Scaffold(
-        topBar = { AppBar() },
-        floatingActionButton = { HomeFloatingActionButton( onClickAdd = onClickAddNote ) }
+        topBar = {
+            AppBar(
+                updateShowMenu = { isMenuOpen = !isMenuOpen },
+                isMenuOpen = isMenuOpen
+            )
+        },
+        floatingActionButton = {
+            HomeFloatingActionButton(
+                onClickAdd = onClickAddNote
+            )
+        }
     ) {
         //EmptyNoteList()
         if (isListEmpty) {
@@ -46,7 +54,8 @@ fun HomeScreen(
 }
 
 @Composable
-fun AppBar() {
+fun AppBar(updateShowMenu: () -> Unit, isMenuOpen: Boolean) {
+    val context = LocalContext.current
     TopAppBar(
         navigationIcon = {
             IconButton(onClick = { /*TODO*/ }) {
@@ -60,6 +69,39 @@ fun AppBar() {
         },
         title = {
             Text("Home Screen")
+        },
+        actions = {
+            //Box {
+                IconButton(onClick = updateShowMenu) {
+                    Icon(
+                        imageVector = Icons.Rounded.MoreVert,
+                        contentDescription = "settings icon",
+                        modifier = Modifier.padding(horizontal = 12.dp)
+                    )
+
+                }
+            //}
+            DropdownMenu(
+                expanded = isMenuOpen,
+                onDismissRequest = updateShowMenu,
+                modifier = Modifier.padding(5.dp)
+            ) {
+                DropdownMenuItem(
+                    onClick = {
+                        Toast.makeText(
+                            context,
+                            "Toast",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                ) {
+                    Text(text = "Toast")
+                }
+                DropdownMenuItem(onClick = { /*TODO*/ }) {
+                    Text(text = "Order")
+                }
+            }
+
         }
     )
 }
@@ -80,7 +122,7 @@ fun EmptyNoteList() {
         modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
-        ) {
+    ) {
         Text(
             text = "Tap + to write a new note!",
             style = MaterialTheme.typography.h6,
@@ -90,47 +132,26 @@ fun EmptyNoteList() {
 
 @Composable
 fun NoteList(notes: State<List<Note>?>, onNoteClick: (String) -> Unit = {}) {
-        LazyColumn {
-            notes.value?.size?.let {
-                items(it) {
-                    Note(note = notes.value!![it], onNoteClick = onNoteClick)
+    LazyColumn {
+        notes.value?.size?.let {
+            items(it) {
+                Note(note = notes.value!![it], onNoteClick = onNoteClick)
 
-                }
             }
         }
     }
+}
 
 @Composable
 fun Note(note: Note, onNoteClick: (String) -> Unit) {
     Surface(modifier = Modifier.clickable { onNoteClick(note.id.toString()) }) {
         Column(modifier = Modifier.padding(8.dp)) {
-            Text(text = note.noteTitle, style = MaterialTheme.typography.subtitle2 )
+            Text(text = note.noteTitle, style = MaterialTheme.typography.subtitle2)
             Text(text = note.noteContent, style = MaterialTheme.typography.body2)
             Divider()
         }
     }
 
 }
-/*
-@Preview
-@Composable
-fun NoteListPreview() {
-    NotepadTheme() {
-        NoteList()
-    }
 
-}
-
- */
-
-
-/*
-@Preview(showSystemUi = true)
-@Composable
-fun HomeScreenPreview() {
-    NotepadTheme {
-        HomeScreen()
-    }
-}
- */
 
