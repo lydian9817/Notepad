db64e0b3b17497204ce94be7e223668db151870d
mauri <mauricio.caricari@gmail.com>
2022-11-27 22:40:20 -0300

    Added function to keep the order type (title or date) when changing the sort order to ascending or descending

--- app/src/main/java/com/example/notepad/domain/util/NoteOrder.kt
+++ app/src/main/java/com/example/notepad/domain/util/NoteOrder.kt
@@ -3,4 +3,13 @@ package com.example.notepad.domain.util
 sealed class NoteOrder(val orderType: OrderType) {
     class Title (orderType: OrderType): NoteOrder(orderType)
     class Date (orderType: OrderType): NoteOrder(orderType)
+
+    // Function to keep the order type (title or date) when changing the
+    // sort order to ascending or descending
+    fun copy(orderType: OrderType): NoteOrder {
+        return when (this) {
+            is Title -> Title(orderType)
+            is Date -> Date(orderType)
+        }
+    }
 }
