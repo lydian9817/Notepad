a2f5f8d6fd303cc2aae455f4495dbe4250fa6c8b
mauri <mauricio.caricari@gmail.com>
2022-11-29 17:49:14 -0300

    Now id can be null

--- app/src/main/java/com/example/notepad/domain/model/Note.kt
+++ app/src/main/java/com/example/notepad/domain/model/Note.kt
@@ -7,7 +7,7 @@ import javax.annotation.Nonnull
 
 @Entity(tableName = "note_table")
 data class Note(
-    @PrimaryKey(autoGenerate = true) val id: Int = 0,
+    @PrimaryKey(autoGenerate = true) val id: Int? = 0,
     @Nonnull @ColumnInfo(name = "note_title") val noteTitle: String,
     @Nonnull @ColumnInfo(name = "note_content") val noteContent: String,
     @Nonnull @ColumnInfo(name = "note_timestamp") val noteTimestamp: Long,
