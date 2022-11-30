ec2a66437de44033e5bbd0d8be1d29a26901eb8e
mauri <mauricio.caricari@gmail.com>
2022-11-30 20:57:06 -0300

    Updated NavHost composable

--- app/src/main/java/com/example/notepad/navigation/NotepadNavHost.kt
+++ app/src/main/java/com/example/notepad/navigation/NotepadNavHost.kt
@@ -1,83 +1,46 @@
 package com.example.notepad.navigation
 
-import android.util.Log
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
-import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.navArgument
-import com.example.notepad.presentation.home.HomeViewModel
-import com.example.notepad.presentation.edit.EditScreen
 import com.example.notepad.presentation.home.HomeScreen
+import com.example.notepad.presentation.util.Screen
 
 @Composable
 fun NotepadNavHost(navController: NavHostController) {
     NavHost(
         navController = navController,
-        startDestination = HomeScreen.route
+        startDestination = Screen.HomeScreen.route
     ) {
-        composable(route = HomeScreen.route) {
-            val homeViewModel = hiltViewModel<HomeViewModel>() //viewModel
-            homeViewModel.resetStateValues()
+        composable(route = Screen.HomeScreen.route) {
             HomeScreen(
                 onClickAddNote = {
-                    navController.navigate(EditScreen.route + "/noteId") {
-                        launchSingleTop = true
-                    }
+                    navController.navigate(Screen.EditScreen.route)
                 },
                 onNoteClick = { id ->
-                    navController.navigate(EditScreen.route + "/$id") {
-                        Log.i("route", "route is ${EditScreen.route + '/'}$id")
-                        launchSingleTop = true
-                    }
-                },
-                viewModel = homeViewModel
+                    navController.navigate(
+                        Screen.EditScreen.route +
+                            "?noteId=$id")
+                }
             )
         }
-        composable(
-            route = EditScreen.route + "/{noteId}",
-            arguments = listOf(
-                navArgument("noteId") {
-                    type = NavType.StringType
-                    defaultValue = "0"
-                    nullable = true
 
+        composable(
+            route = Screen.EditScreen.route +
+                    "?noteId={noteId}",
+            arguments = listOf(
+                navArgument(
+                    name = "noteId"
+                ) {
+                    type = NavType.IntType
+                    defaultValue = -1
                 }
             )
         ) {
-            //assign parent's (HomeScreen's) viewModel
-            val parentEntry = remember(it) {
-                navController.getBackStackEntry(HomeScreen.route)
-            }
-            val parentViewModel = hiltViewModel<HomeViewModel>(
-                parentEntry
-            )
-            val nota = it.arguments?.getString("noteId")
-            if (nota != null && nota != "noteId") {
-                Log.i("nota", "el id es ${nota.toInt()}")
-            }
 
-            EditScreen(
-                onClickSaveNote = {
-                    navController.navigate(HomeScreen.route) {
-                        launchSingleTop = true
-                        popUpTo(HomeScreen.route)
-                    }
-                    //saves the note
-                    parentViewModel.addNewNote(parentViewModel.noteTitle, parentViewModel.noteContent)
-                },
-                selectedNoteId = it.arguments?.getString("noteId"),
-                homeViewModel = parentViewModel
-            )
         }
     }
 }
-
-
-/*
-fun NavHostController.navigateSingleTopTo(route: String) =
-    this.navigate(route) { launchSingleTop = true } //launch a single copy of the destination
- */
