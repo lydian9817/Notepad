a6ad4b8cda81eadfae28c26bd65c8fa331d09eff
mauri <mauricio.caricari@gmail.com>
2022-11-30 21:21:07 -0300

    Updated NavHost composable

--- app/src/main/java/com/example/notepad/navigation/NotepadNavHost.kt
+++ app/src/main/java/com/example/notepad/navigation/NotepadNavHost.kt
@@ -6,6 +6,7 @@ import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.navArgument
+import com.example.notepad.presentation.edit.EditScreen
 import com.example.notepad.presentation.home.HomeScreen
 import com.example.notepad.presentation.util.Screen
 
@@ -40,7 +41,11 @@ fun NotepadNavHost(navController: NavHostController) {
                 }
             )
         ) {
-
+            EditScreen(
+                onClickSaveNote = {
+                    navController.navigateUp()
+                }
+            )
         }
     }
 }
