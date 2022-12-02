566421d0248e07f89ab9ebfdedcd3138ed38b2ed
mauri <mauricio.caricari@gmail.com>
2022-12-02 17:48:42 -0300

    Updated HintTextField.kt to m3

--- app/src/main/java/com/example/notepad/presentation/edit/components/HintTextField.kt
+++ app/src/main/java/com/example/notepad/presentation/edit/components/HintTextField.kt
@@ -2,14 +2,16 @@ package com.example.notepad.presentation.edit.components
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material.Text
-import androidx.compose.material.TextField
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusState
 import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.text.TextStyle
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HintTextField(
     text: String,
