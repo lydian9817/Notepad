1fbb03fd4ca7ef38ff348069ee6bddafd46ac664
mauri <mauricio.caricari@gmail.com>
2022-12-06 15:57:20 -0300

    Put the canvas inside a box

--- app/src/main/java/com/example/notepad/presentation/home/components/SelectCircle.kt
+++ app/src/main/java/com/example/notepad/presentation/home/components/SelectCircle.kt
@@ -1,21 +1,30 @@
 package com.example.notepad.presentation.home.components
 
 import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun SelectCircle() {
-    Canvas(modifier = Modifier.fillMaxSize()) {
-        val canvasWidth = size.width
-        val canvasHeight = size.height
-        drawCircle(
-            color = Color.Gray,
-            center = Offset(x = canvasWidth / 2, y = canvasHeight / 2),
-            radius = canvasWidth / 2
-        )
+    Box(
+        modifier = Modifier.height(24.dp).width(24.dp)
+    ) {
+        Canvas(modifier = Modifier.fillMaxSize()) {
+            val canvasWidth = size.width
+            val canvasHeight = size.height
+            drawCircle(
+                color = Color.Gray,
+                center = Offset(x = canvasWidth / 2, y = canvasHeight / 2),
+                radius = canvasWidth / 2
+            )
+        }
     }
+
 }
