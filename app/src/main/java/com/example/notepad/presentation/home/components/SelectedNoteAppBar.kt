fdf05ac368a2f3d63df0bbca773f84b6918259c8
mauri <mauricio.caricari@gmail.com>
2022-12-06 16:23:07 -0300

    Added a delete IconButton

--- app/src/main/java/com/example/notepad/presentation/home/components/SelectedNoteAppBar.kt
+++ app/src/main/java/com/example/notepad/presentation/home/components/SelectedNoteAppBar.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Close
+import androidx.compose.material.icons.rounded.Delete
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -28,6 +29,15 @@ fun SelectedNoteAppBar(
         },
         title = {
             Text("Selected")
+        },
+        actions = {
+            IconButton(onClick = { /*TODO*/ }) {
+                Icon(
+                    imageVector = Icons.Rounded.Delete,
+                    contentDescription = "Delete Selected notes",
+                    modifier = Modifier.padding(horizontal = 12.dp)
+                )
+            }
         }
     )
 }
