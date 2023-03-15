dfd3e45dca4e5c647a2193c0d9264e3ad7684d22
mauri <mauricio.caricari@gmail.com>
2023-03-15 17:44:52 -0300

    Added settings screen

---
+++ app/src/main/java/com/example/notepad/presentation/settings/SettingsScreen.kt
@@ -0,0 +1,8 @@
+package com.example.notepad.presentation.settings
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun SettingsScreen() {
+    
+}
--- app/src/main/java/com/example/notepad/presentation/util/Screen.kt
+++ app/src/main/java/com/example/notepad/presentation/util/Screen.kt
@@ -3,4 +3,5 @@ package com.example.notepad.presentation.util
 sealed class Screen(val route: String) {
     object HomeScreen: Screen("home_screen")
     object EditScreen: Screen("edit_screen")
+    object  SettingsScreen: Screen("settings_screen")
 }
