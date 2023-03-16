c3f8219db1e83c3a3921537f8066ca6183fd64a5
mauri <mauricio.caricari@gmail.com>
2023-03-15 21:22:48 -0300

    Added String resource

--- app/src/main/java/com/example/notepad/presentation/settings/components/SettingsAppBar.kt
+++ app/src/main/java/com/example/notepad/presentation/settings/components/SettingsAppBar.kt
@@ -4,6 +4,8 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
+import com.example.notepad.R
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -12,7 +14,7 @@ fun SettingsTopAppBar(
 ) {
     TopAppBar(
         title = {
-            Text(text = "Settings")
+            Text(text = stringResource(R.string.settings_screen_appbar_title))
         },
         navigationIcon = {
             IconButton(
